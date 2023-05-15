b8ea8afe9889db207200941054f7d054ac7114ca
Diego Peña Vicente <diego10azul@hotmail.com>
2023-05-15 12:19:02 +0200

    fix ref lenguage file

--- src/lib/i18n.ts
+++ src/lib/i18n.ts
@@ -2,7 +2,7 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import i18nBackend from "i18next-http-backend";
 
-const getCurrentHost = import.meta.env.MODE === 'development' ? 'http://localhost:5173' : 'https://lanonnarose.com/'
+const getCurrentHost = import.meta.env.MODE === 'development' ? 'http://localhost:5173' : 'https://diegopenavicente.com/'
 
 i18n
     .use(i18nBackend)
